dccd8c35c183de23ed1f5af261b075888bf0b0af
Jamille Silvestre <pjdeesilvestre@gmail.com>
2020-02-03 20:28:19 +0800

    added design for topnav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Logo from './assets/images/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus, faSearch } from '@fortawesome/free-solid-svg-icons'
+import './assets/scss/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        
+    <div className="main-container">
+      <div className="top-nav">
+        <img src={Logo} height="40"/>
+        <div className="menu">
+          <div className="menu-item">
+            Home
+          </div>
+          <div className="menu-item">
+            Catalog
+          </div>
+          <div className="menu-item">
+            Shop
+          </div>
+          <div className="menu-item">
+            Pages
+          </div>
+        </div>
+        <div className="menu-right">
+          <FontAwesomeIcon icon={faSearch} className="list-icon" />
+          <FontAwesomeIcon icon={faCartPlus} className="list-icon" />
+        </div>
+      </div>
+      <div className="product-container">
+
       </div>
     </div>
   );
